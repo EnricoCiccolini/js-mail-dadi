2ed18926c618c07bfaf1c4a416508e7ad83ca312
EnricoCiccolini <piroroma90@gmail.com>
2025-02-27 15:22:26 +0100

    feat: complete bonus dice

---
+++ dadi -bonus/script.js
@@ -0,0 +1,25 @@
+/* 
+Generare un numero random da 1 a 6, sia per il giocatore sia per il computer.
+Stabilire il vincitore, in base a chi fa il punteggio più alto.
+
+bonus Invece di generare il numero random per l'utente, chiedilo con un prompt!
+*/
+
+let computer                                    // dichiaro le variabili in cui ospiero il numero casuale
+let player                                      // dichiaro la variabile in cui ospitero il numero dell' utente 
+
+computer = Math.random () *(6 - 1 ) +1                              // creo il tiro casuale per il banco
+computer = parseInt(computer)                                       // lo trasformo in numeri senza la virgola
+console.log(`Il banco ha tirato ${computer}`)                       // dichiaro all ' utente uanto ha fatto il banco 
+
+
+player = prompt(`inserisci un numero da 1 a 6`)                      // chiedo all'utente un numero da 1 a 6
+player = parseInt(player)                                            // controllo che sia un numero intero trasformandolo in numero senza virgola 
+
+if(isNaN(player)) console.log(`il numero inserito non è valido` )      // se il valore è nan dico che il valore non è valido 
+    else{                                                               // se è valido continuo con il programma 
+        if(computer > player) console.log(` il banco ha vinto `)              // confronto i due risultati se a secodso di chi ha fatto il tiro maggiore inoltro il risultato 
+        else if (player > computer) console.log(` il giocatore ha vinto `)
+        else console.log(` PAREGGIO `)                                         // contemplo anche il pareggio nelle ipotesi
+
+}
--- mail/script.js
+++ mail/script.js
@@ -8,13 +8,13 @@ Nota:
 Non è necessario provvedere alla validazione delle email 
 */
 
-const Mail = ['enrico@tiscali.it', 'ciccolini@gmail.com', 'gaetano@aruba.com', 'alessio@yahoo.it', 'stefano@blizard.com', 'prova@prova.com'] // creo un array di email 
+const mail = ['enrico@tiscali.it', 'ciccolini@gmail.com', 'gaetano@aruba.com', 'alessio@yahoo.it', 'stefano@blizard.com', 'prova@prova.com'] // creo un array di email 
 
 const mailCeck = prompt(`Ciao inserisci l email da controllare`)            // chiedo all' utente l email da controllare e la salvo in una variabile 
 let control = ('non è presente')                                            // creo una variabile di controllo a cui assegno non presente di default
 for (let i = 0; i < mailCeck.length; i++) {                                 // creo un ciclo con un idice uguale alle email inserite nella variabile mail 
  
-    if (mailCeck === Mail[i]) control = (`è presente`)                       //controllo se la  email inserita è presente se presente cambio il valore di controllo
+    if (mailCeck === mail[i]) control = (`è presente`)                       //controllo se la  email inserita è presente se presente cambio il valore di controllo
 }
 
 console.log(` La tua email  ${control} in lista `)                            // mando un messaggio all' utente riferendo il risultato 
