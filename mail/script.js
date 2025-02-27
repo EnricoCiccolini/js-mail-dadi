32b06c101da7492fb790839034de924a14e8a8ca
EnricoCiccolini <piroroma90@gmail.com>
2025-02-27 15:46:11 +0100

    fixed : variable control

--- mail/script.js
+++ mail/script.js
@@ -10,11 +10,12 @@ Non è necessario provvedere alla validazione delle email
 
 const mail = ['enrico@tiscali.it', 'ciccolini@gmail.com', 'gaetano@aruba.com', 'alessio@yahoo.it', 'stefano@blizard.com', 'prova@prova.com'] // creo un array di email 
 
+
 const mailCeck = prompt(`Ciao inserisci l email da controllare`)            // chiedo all' utente l email da controllare e la salvo in una variabile 
 let control = ('non è presente')                                            // creo una variabile di controllo a cui assegno non presente di default
 if( mailCeck === '') console.log (`La tua email  ${control} in lista`)      // creo un controllo per contemplare il caso in cui l-utente lasci il campo vuoto 
     else{
-for (let i = 0; i < mailCeck.length; i++) {                                 // creo un ciclo con un idice uguale alle email inserite nella variabile mail 
+for (let i = 0; i < mail.length; i++) {                                 // creo un ciclo con un idice uguale alle email inserite nella variabile mail 
  
     if (mailCeck === mail[i]) control = (`è presente`)                       //controllo se la  email inserita è presente se presente cambio il valore di controllo
 }
